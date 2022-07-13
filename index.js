d60991fe73a21883fd397cc2d847537375b16084
Belen Urbinelli <belen.urbinelli@gmail.com>
2022-07-13 17:58:43 -0300

    simulador interactivo

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const respuesta = prompt("Ingrese mayor, si desea saber a partir de que edad es mayor o menor, en caso contrario")
+function edadMayorOMenor ( numero, mayorOMenor ) {
 
-for (let i = 1; i <= 100; i++){
+for(let i = 1; i <= numero; i++){
 
-    if ( respuesta === "mayor" && i >= 18 ){
-        console.log(`Si usted tiene ${i} años, es mayor`)
-    }
-    else if ( respuesta === "menor" && i < 18){
-        console.log(`Si usted tiene ${i} años, es menor`)
-    }
-    else if ( respuesta === null){
-        console.log("No ingresó ninguna instrucción coherente")
-        break;
-    }  
-
+    if ( mayorOMenor === "mayor" && i >= 18 ){
+    console.log(i)
 }
+    else if ( mayorOMenor === "menor" && i < 18){
+    console.log(i)
+}
+}
+}
+
+edadMayorOMenor (8, "menor")
+edadMayorOMenor (18, "mayor")
